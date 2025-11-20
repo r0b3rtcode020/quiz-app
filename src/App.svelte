398bdfa3de16b608232dce52249f91bf9f0c50f5
r0b3rtcode020 <rob.artiga@hotmail.com>
2025-11-20 11:46:43 -0600

    feat: Set up quiz app with routing and basic pages

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router, Route } from "svelte5-router";
+  import QuizSelection from "./pages/QuizSelection.svelte";
+  import Quiz from "./pages/Quiz.svelte";
+  import Result from "./pages/Result.svelte";
+</script>
+
+<Router>
+  <Route path="/" component={QuizSelection} />
+  <Route path="/quiz" component={Quiz} />
+  <Route path="/result" component={Result} />
+</Router>
