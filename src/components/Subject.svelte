a23b58d4038f767d231afb6f45419791c328278e
r0b3rtcode020 <rob.artiga@hotmail.com>
2025-11-23 02:20:26 -0600

    refactor: extract Subject link into reusable component

---
+++ src/components/Subject.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { link } from "svelte5-router";
+
+  let { href, text, icon } = $props();
+</script>
+
+<a {href} use:link>
+  <figure>
+    <img src={icon} alt={text} />
+  </figure>
+  <span>{text}</span>
+</a>
--- src/pages/QuizSelection.svelte
+++ src/pages/QuizSelection.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { link } from "svelte5-router";
   import { quizStore } from "../store/store.svelte.js";
+  import Subject from "../components/Subject.svelte";
 </script>
 
 <h1>Welcome to the <span>Frontend Quiz!</span></h1>
 <p>Pick a subject to get started.</p>
 
-{#each quizStore.quizzes as { title }, i (i)}
-  <a href={`/quiz/${title.toLowerCase()}`} use:link>{title}</a>
+{#each quizStore.quizzes as { title, icon }, i (i)}
+  <Subject href={`/quiz/${title.toLowerCase()}`} text={title} {icon} />
 {/each}
