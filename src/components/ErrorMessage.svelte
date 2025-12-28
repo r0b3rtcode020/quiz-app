dd033de4eac7b694c5d1ce9a20aaaf5a18e6092d
r0b3rtcode020 <rob.artiga@hotmail.com>
2025-12-28 06:27:35 -0600

    style: enhance feedback components with error styling and icons

--- src/components/ErrorMessage.svelte
+++ src/components/ErrorMessage.svelte
@@ -5,6 +5,31 @@
 </script>
 
 <div>
-  <img src={IconError} alt="Error message" />
+  <figure>
+    <img src={IconError} alt="Error message" />
+  </figure>
   <p>{message}</p>
 </div>
+
+<style>
+  div {
+    display: flex;
+    align-items: center;
+    gap: var(--sp-8);
+  }
+
+  figure {
+    width: clamp(var(--sp-32), 6vw, var(--sp-40));
+    aspect-ratio: 1;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  p {
+    color: var(--clr-error);
+    font-weight: var(--fw-regular);
+    line-height: var(--lh-150);
+  }
+</style>
