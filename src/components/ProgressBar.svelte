7ff7209de56dd5fb32136000c11bd66f34976da7
r0b3rtcode020 <rob.artiga@hotmail.com>
2025-12-30 06:01:30 -0600

    feat: add progress bar component

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -1,1 +1,31 @@
-<div>Progress bar</div>
+<script>
+  let { current, total } = $props();
+</script>
+
+<progress value={current} max={total}></progress>
+
+<style>
+  progress {
+    width: 100%;
+    height: var(--sp-16);
+    border-radius: 63rem;
+    appearance: none;
+    -webkit-appearance: none;
+  }
+
+  progress::-webkit-progress-bar {
+    background-color: var(--subject-bg);
+    padding: var(--sp-4);
+    border-radius: 63rem;
+  }
+
+  progress::-webkit-progress-value {
+    background-color: var(--clr-purple-600);
+    border-radius: 63rem;
+  }
+
+  progress::-moz-progress-bar {
+    background-color: var(--clr-purple-600);
+    border-radius: 63rem;
+  }
+</style>
