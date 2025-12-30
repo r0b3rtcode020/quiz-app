0331e8c76b392417a942ade4e2d80d4c79bc6174
r0b3rtcode020 <rob.artiga@hotmail.com>
2025-12-30 06:22:59 -0600

    refactor: centralize Switch and SubjectSelected components in App.svelte, simulate light/dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,28 @@
 <script>
   import { Router, Route } from "svelte5-router";
+  import { quizStore } from "./store/store.svelte";
   import QuizSelection from "./pages/QuizSelection.svelte";
   import Quiz from "./pages/Quiz.svelte";
   import Result from "./pages/Result.svelte";
+  import SubjectSelected from "./components/SubjectSelected.svelte";
+  import Switch from "./components/Switch.svelte";
+
+  let isDark = $state(false);
+  let theme = $derived(isDark ? "dark" : "light");
+
+  $effect(() => document.documentElement.setAttribute("data-theme", theme));
 </script>
 
 <Router>
-  <Route path="/" component={QuizSelection} />
-  <Route path="/quiz/:subject/:questionIndex" component={Quiz} />
-  <Route path="/result" component={Result} />
+  <header>
+    {#if quizStore?.currentSubject}
+      <SubjectSelected subject={quizStore?.currentSubject} icon={quizStore?.currentIcon} />
+    {/if}
+    <Switch bind:isDark />
+  </header>
+  <main>
+    <Route path="/" component={QuizSelection} />
+    <Route path="/quiz/:subject/:questionIndex" component={Quiz} />
+    <Route path="/result" component={Result} />
+  </main>
 </Router>
