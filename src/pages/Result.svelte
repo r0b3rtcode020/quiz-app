0c3e2651f5fcdc1122e83cd6220d62f7d104af5d
r0b3rtcode020 <rob.artiga@hotmail.com>
2026-01-02 04:17:43 -0600

    style: add layout of Result view

--- src/pages/Result.svelte
+++ src/pages/Result.svelte
@@ -17,11 +17,40 @@
   };
 </script>
 
-<Title text="Quiz completed" boldText="You scored..." />
-<ScoreCard
-  score={quizStore?.score}
-  totalQuestions={quizStore?.totalQuestions}
-  subject={quizStore?.currentSubject}
-  icon={quizStore?.currentIcon}
-/>
-<Button label="Play again" onClick={handlePlayAgain} />
+<section>
+  <div class="heading">
+    <Title text="Quiz completed" boldText="You scored..." />
+  </div>
+  <div class="result">
+    <ScoreCard
+      score={quizStore?.score}
+      totalQuestions={quizStore?.totalQuestions}
+      subject={quizStore?.currentSubject}
+      icon={quizStore?.currentIcon}
+    />
+    <Button label="Play again" onClick={handlePlayAgain} />
+  </div>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: clamp(var(--sp-40), calc(0.55rem + 8.27vw), var(--sp-128));
+
+    @media (width >= 80rem) {
+      flex-direction: row;
+    }
+  }
+
+  .heading {
+    flex: 1;
+  }
+
+  .result {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: clamp(var(--sp-16), 4.25vw, var(--sp-32));
+  }
+</style>
